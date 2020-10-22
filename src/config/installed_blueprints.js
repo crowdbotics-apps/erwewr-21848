b05711f72a7a82224c36a5594dc4a851389dcf56
Crowdbotics <team@crowdbotics.com>
2020-10-22 23:26:30 +0000

    add package blueprint-react-native-settings-screen

--- src/config/installed_blueprints.js
+++ src/config/installed_blueprints.js
@@ -1,5 +1,6 @@
 export const installed_blueprints = [
   //@BlueprintInsertion
+{ name: 'Settings5156369', human_name: 'Settings 5', access_route: 'Settings5156369'},
 { name: 'Messaging6156368', human_name: 'Messaging 6', access_route: 'Messaging6156368', icon: 'comments'},
 { name: 'NotificationList7156367', human_name: 'Notification List 7', access_route: 'NotificationList7156367'},
 { name: 'Camera9156365', human_name: 'Camera 9', access_route: 'Camera9156365', icon: 'camera'},
--- src/navigator/mainNavigator.js
+++ src/navigator/mainNavigator.js
@@ -5,6 +5,7 @@ import {createDrawerNavigator} from 'react-navigation-drawer';
 import SplashScreen from "../features/SplashScreen";
 import SideMenu from './sideMenu';
 //@BlueprintImportInsertion
+import Settings5156369Navigator from '../features/Settings5156369/navigator';
 import Messaging6156368Navigator from '../features/Messaging6156368/navigator';
 import NotificationList7156367Navigator from '../features/NotificationList7156367/navigator';
 import Camera9156365Navigator from '../features/Camera9156365/navigator';
@@ -16,6 +17,7 @@ import Camera9156365Navigator from '../features/Camera9156365/navigator';
 const AppNavigator = {
 
     //@BlueprintNavigationInsertion
+Settings5156369: { screen: Settings5156369Navigator },
 Messaging6156368: { screen: Messaging6156368Navigator },
 NotificationList7156367: { screen: NotificationList7156367Navigator },
 Camera9156365: { screen: Camera9156365Navigator },
